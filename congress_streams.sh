6a74186ad9897c67bd7f1c385a08e4199673578b
Andreas Geisenhainer <psycorama@opensecure.de>
2014-12-26 14:12:54 +0100

    update for 31C3
        * streams adjusted
        * selection for stream type:  RTMP, HLS, WEBM
        * HD/SD selection

--- congress_streams.sh
+++ congress_streams.sh
@@ -3,14 +3,31 @@
 ######################################################################
 
 # stream URL configuration be here
-# official streams as statde here: https://events.ccc.de/congress/2012/wiki/Streaming
-STREAM_BASE=http://wmv.29c3.fem-net.de
-STREAM_1=${STREAM_BASE}/saal1
-STREAM_2=${STREAM_BASE}/saal4
-STREAM_3=${STREAM_BASE}/saal6
+# official streams as stated here:
+#   https://events.ccc.de/congress/2014/wiki/Streams
+#   http://streaming.media.ccc.de/
+
+if [ -z $(which mplayer) ]; then
+    echo "mplayer is needed for this, please install one"
+    exit -1;
+fi
+
+RTMP_BASE=rtmp://rtmp.stream.c3voc.de:1935
+HLS_BASE=http://hls.stream.c3voc.de/hls
+WEBM_BASE=http://webm.stream.c3voc.de:8000
+
+if [ -z "$QUALITY" ]; then
+    QUALITY=hd
+    export QUALITY
+fi
+
+if [ -z "$STREAM_TYPE" ] ; then
+    STREAM_TYPE=webm
+    export STREAM_TYPE
+fi
 
 # Fahrplan URL
-FAHRPLAN=http://events.ccc.de/congress/2012/Fahrplan/schedule.de.xml
+FAHRPLAN=http://events.ccc.de/congress/2014/Fahrplan/schedule.xml
 
 # mplayer options
 MPLAYER_OPTS="-cache 4096"
@@ -29,34 +46,65 @@ while true; do
 
     SCHEDULE=$(${MYPATH}/parse_fahrplan.pl)
     TIME=`date +%H:%M`
-    xmessage -buttons "Saal 1":1,"Saal 4":4,"Saal 6":6,"reload":9,"Quit":0 \
+    xmessage -buttons "Saal 1":1,"Saal 2":2,"Saal G":3,"Saal 6":4,\
+"set RTMP":21,"set HLS":22,"set WEBM":23,\
+"set HD":24,"set SD":25,\
+"reload":9,"Quit":0 \
         -default Cancel \
-        -center "Miniauswahlskript fuer die Streams vom
-Chaos Communication Congress.
+        -center "small script for easy selection of streams from
+    31st Chaos Communication Congress
 
-Uhrzeit: ${TIME}
+streams available via http://streaming.media.ccc.de/
+
+now: ${TIME}
 
 ${SCHEDULE}
 
+stream options: stream_type=$STREAM_TYPE, quality=$QUALITY
 mplayer options: ${MPLAYER_OPTS}"
 
-    case $? in
-	1)
-	    mplayer ${MPLAYER_OPTS} ${STREAM_1}
-	    ;;
+    SELECT=$?
+    case $SELECT in
+    [1-4])
+        case $STREAM_TYPE in
+        "rtmp")
+            STREAM_BASE="${RTMP_BASE}/stream/s"
+            STREAM_TAIL="_native_$QUALITY"
+            ;;
+        "hls")
+            STREAM_BASE="${HLS_BASE}/s"
+            STREAM_TAIL="_native_$QUALITY.m3u8"
+            ;;
+        "webm")
+            STREAM_BASE="${WEBM_BASE}/s"
+            STREAM_TAIL="_native_$QUALITY.webm"
+            ;;
+        esac
 
-	4)
-	    mplayer ${MPLAYER_OPTS} ${STREAM_2}
-	    ;;
-	6)
-	    mplayer ${MPLAYER_OPTS} ${STREAM_3}
-	    ;;
-	9)
+        mplayer ${MPLAYER_OPTS} ${STREAM_BASE}$SELECT$STREAM_TAIL
+        ;;
+    21)
+        STREAM_TYPE=rtmp
+        ;;
+    22)
+        STREAM_TYPE=hls
+        ;;
+    23)
+        STREAM_TYPE=webm
+        ;;
+    24)
+        QUALITY=hd;
+        ;;
+    25)
+        QUALITY=sd;
+        ;;
+    9)
 	    exec ${MYPATH}/congress_streams.sh
 	    ;;
-	0)
-	    exit 0
-	    ;;
+    0)
+        exit 0
+        ;;
     esac
+
 
 done
