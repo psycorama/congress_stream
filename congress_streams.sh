a8f31942adb230be54349e38f45296af256a71fa
Christian Garbs <mitch@cgarbs.de>
2020-12-27 21:40:28 +0100

    quote the whole button string, not just the label

--- congress_streams.sh
+++ congress_streams.sh
@@ -93,10 +93,11 @@ while true; do
     SCHEDULE=$(${MYPATH}/parse_fahrplan.pl)
     TIME=$(date +%H:%Mh)
     xmessage -xrm '*international: true' \
-        -buttons "rC1":11,"rC2":12,\
-"set HLS":22,"set WEBM":23,\
-"set HD":24,"set SD":25,\
-"reload":9,"Quit":0 \
+             -buttons \
+'rC1:11','rC2:12',\
+'set HLS:22','set WEBM:23',\
+'set HD:24','set SD:25',\
+'reload:9','Quit:0' \
         -default Cancel \
         -center "small script for easy selection of streams from
     $TITLE
